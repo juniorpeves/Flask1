d171d803d8dfe3fc18e7c04f07c9db71366fa543
juniorpeves <juniorpeves@gmail.com>
2021-12-30 08:34:43 -0500

    clase33_eliminar-tarea

--- app/forms.py
+++ app/forms.py
@@ -7,3 +7,12 @@ class LoginForm(FlaskForm):
     username = StringField('Nombre de usuario', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Enviar')
+
+
+class TodoForm(FlaskForm):
+    description = StringField('Descripción', validators=[DataRequired()])
+    submit = SubmitField('Crear')
+
+
+class DeleteTodoForm(FlaskForm):
+    submit = SubmitField('Borrar')
--- main.py
+++ main.py
@@ -2,10 +2,9 @@ import unittest
 from flask import request, make_response, redirect, render_template, session, url_for, flash
 from flask_login import login_required, current_user
 
-
 from app import create_app
-from app.forms import LoginForm
-from app.firestore_service import get_users, get_todos
+from app.forms import TodoForm, DeleteTodoForm
+from app.firestore_service import get_users, get_todos, put_todo, delete_todo
 
 app = create_app()
 
@@ -36,17 +35,35 @@ def index():
     return response
 
 
-@app.route('/hello', methods=['GET'])
+@app.route('/hello', methods=['GET', 'POST'])
 @login_required
 def hello():
     user_ip = session.get('user_ip')
     username = current_user.id
+    todo_form = TodoForm()
+    delete_form = DeleteTodoForm()
 
     context = {
         'user_ip': user_ip,
         'todos': get_todos(user_id=username),
-        'username': username
+        'username': username,
+        'todo_form': todo_form,
+        'delete_form': delete_form,
     }
+
+    if todo_form.validate_on_submit():
+        put_todo(user_id=username, description=todo_form.description.data)
+
+        flash('Tu tarea se creo con éxito!')
+
+        return redirect(url_for('hello'))
 
     return render_template('hello.html', **context)
 
+
+@app.route('/todos/delete/<todo_id>', methods=['POST'])
+def delete(todo_id):
+    user_id = current_user.id
+    delete_todo(user_id=user_id, todo_id=todo_id)
+
+    return redirect(url_for('hello'))
